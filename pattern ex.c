aadcb2ccc9f3f9a2896f3c4e2610a4b969ea096b
MHMD AMIIN <162644818+mhmdamin1@users.noreply.github.com>
2024-07-15 19:42:29 -0500

    Add files via upload

---
+++ pattern ex.c
@@ -0,0 +1,21 @@
+//mohamed amin mohamed
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i , j ;
+
+    for(i=0; i<=9 ; i++){
+
+        for(j=i; j<=9 ; j++){
+
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
